e9d6c343df4e4fc78edb87cdbeed7ee64146a6f5
Mritunjay Kumar <83032964+Mritunjay1711@users.noreply.github.com>
2023-04-23 15:11:00 +0530

    update spinner

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import loading from '../assets/loading.gif'
-const Spinner = ()=> {
-    return (
-        <div className='text-center'>
-           <img className="my-3" src={loading} alt="loading"/> 
-        </div>
-    )
-  }
+const Spinner = () => {
+  return (
+    <div className='text-center' style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
+      <img className="my-3" src={loading} alt="Loading..." style={{ width: "100px", height: "100px" }} />
+    </div>
+  )
+}
 
 export default Spinner
